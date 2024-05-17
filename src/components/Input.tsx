5efe6b6295d618180095e34451061ee31bcc5aad
Eduardo Casanova <53999059+ed3899@users.noreply.github.com>
2024-05-17 18:37:16 +0000

    uiux(nextjs): input component added

---
+++ src/components/Input.tsx
@@ -0,0 +1,14 @@
+import { InputProps, Input as NextUiInput } from "@nextui-org/react"
+import React from "react"
+
+/**
+ * @link https://nextui.org/docs/components/input#installation
+ */
+export default function Input(
+  props: Pick<
+    InputProps,
+    "type" | "placeholder" | "labelPlacement" | "endContent"
+  >,
+) {
+  return <NextUiInput {...props} />
+}
