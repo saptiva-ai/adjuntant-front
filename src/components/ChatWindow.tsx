5dddcc8dd3b3792029d00f3ab021d0f8519284eb
Eduardo Casanova <53999059+ed3899@users.noreply.github.com>
2024-05-23 16:42:01 +0000

    uiux(nextjs): chatwindow.tsx added new param and icon image

--- src/components/ChatWindow.tsx
+++ src/components/ChatWindow.tsx
@@ -2,12 +2,14 @@
 
 import { Avatar, Card, CardHeader } from "@nextui-org/react"
 import { Message } from "@/types/message"
+import isEven from "@/util/isEven"
 import { useEffect } from "react"
 
 type ChatWindowProps = {
   messages: Message[]
   msgLimit?: number
   onMsgLimitExceeded?: (...args: any[]) => void
+  topCardHeaderChildren?: () => React.ReactNode
   msgLimitExceededChildren?: () => React.ReactNode
 }
 
@@ -16,7 +18,11 @@ export default function ChatWindow({
   msgLimit = Infinity,
   onMsgLimitExceeded = () => null,
   msgLimitExceededChildren = () => null,
+  topCardHeaderChildren = () => null,
 }: ChatWindowProps) {
+  if (msgLimit !== Infinity && !isEven(msgLimit))
+    throw new Error("msgLimit must be an even number")
+
   const msgsExists = messages.length > 0
   const msgLimitExceeded = messages.length >= msgLimit
 
@@ -26,20 +32,7 @@ export default function ChatWindow({
 
   return (
     <Card className='flex h-96 w-96 flex-col gap-1 overflow-auto overscroll-contain bg-gray-700/80 scrollbar scrollbar-track-gray-200 scrollbar-thumb-green-700 hover:scrollbar-thumb-green-500 active:scrollbar-thumb-green-400'>
-      <CardHeader>
-        <div className='flex gap-1'>
-          <Avatar
-            isBordered
-            showFallback
-            radius='full'
-            size='sm'
-            src='https://nextui.org/avatars/avatar-1.png'
-          />
-          <p className='pt-1.5 text-small font-light leading-none text-default-600'>
-            ¿Como puedo ayudarte hoy?
-          </p>
-        </div>
-      </CardHeader>
+      {topCardHeaderChildren()}
 
       {msgsExists &&
         messages.map(msg => (
@@ -50,7 +43,7 @@ export default function ChatWindow({
                 showFallback
                 radius='full'
                 size='sm'
-                src='https://nextui.org/avatars/avatar-1.png'
+                src={msg.role === "ai" ? "/img/logoVA.webp" : undefined}
               />
               <p className='pt-1.5 text-small font-light leading-none text-default-600'>
                 {msg.text}
